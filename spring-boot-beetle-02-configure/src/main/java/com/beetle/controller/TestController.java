efb48b88226e81123feddab3713e36f6ec26778b
BeetleSpring <1637413223@qq.com>
2021-09-18 08:46:26 +0800

    yaml的使用 和 jasypt的使用 用来配置加密

--- spring-boot-beetle-02-configure/src/main/java/com/beetle/controller/TestController.java
+++ spring-boot-beetle-02-configure/src/main/java/com/beetle/controller/TestController.java
@@ -5,6 +5,7 @@ import com.beetle.bean.Vegetables;
 import com.beetle.configure.FoodConfigure;
 import com.beetle.configure.VegetablesConfigure;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,14 +18,26 @@ public class TestController {
 //    private String name;
 //    @Value("${food.prefix}")
 //    private String prefix;
+    @Value("${info.username}")
+    private String username;
+    @Value("${info.password}")
+    private String password;
 
     @Autowired
     private VegetablesConfigure vegetablesConfigure;
     @RequestMapping("/json")
-    public Vegetables json(){
+    public String json(){
 
-        //Food food = new Food(foodConfigure.getName(),foodConfigure.getPrefix());
+        Food food = new Food(foodConfigure.getName(),foodConfigure.getPrefix());
+
         Vegetables vegetables = new Vegetables(vegetablesConfigure.getPotato(),vegetablesConfigure.getEggplant(),vegetablesConfigure.getGreenPepper());
-        return vegetables;
+        vegetables.setSeasoning(vegetablesConfigure.getSeasoning());
+
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(username);
+        buffer.append(password);
+
+
+        return buffer.toString();
     }
 }
